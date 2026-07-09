922c7bc34480d01efd17ff1017f4f3f98a48be00
kuslapur <bkuslapur@gmail.com>
2026-07-09 16:37:17 +0530

    terraform destroy

---
+++ variable.tf
@@ -0,0 +1,11 @@
+variable "region" {
+  description = "The AWS region to deploy resources in"
+  type        = string
+  default     = "ap-south-1"
+}
+
+variable "instance_type" {
+  description = "The EC2 instance type to use"
+  type        = string
+  default     = "t2.micro"
+}
